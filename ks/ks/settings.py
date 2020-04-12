655431e284fc22fb077f07bb8b14553e489a0e0b
chenzhiy <419083145@qq.com>
2020-04-12 19:54:27 +0800

    修改mysql数据库账号密码

--- ks/ks/settings.py
+++ ks/ks/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 """
 Django settings for ks project.
 
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',#'django.db.backends.sqlite3',
+        'NAME': 'ks',#os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USRE':'root',
+        'PASSWORD':'jerome@135',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
